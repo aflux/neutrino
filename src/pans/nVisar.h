f2f605eca7804cf69ec1d73973dbe9557d72d5d2
tommaso vinci <tommaso.vinci@polytechnique.edu>
2016-04-25 23:02:23 +0200

    nVisar rewritten using qcustomplot

--- src/pans/nVisar.h
+++ src/pans/nVisar.h
@@ -1,7 +1,7 @@
 /*
  *
  *    Copyright (C) 2013 Alessandro Flacco, Tommaso Vinci All Rights Reserved
- * 
+ *
  *    This file is part of neutrino.
  *
  *    Neutrino is free software: you can redistribute it and/or modify
@@ -17,7 +17,7 @@
  *    You should have received a copy of the GNU Lesser General Public License
  *    along with neutrino.  If not, see <http://www.gnu.org/licenses/>.
  *
- *    Contact Information: 
+ *    Contact Information:
  *	Alessandro Flacco <alessandro.flacco@polytechnique.edu>
  *	Tommaso Vinci <tommaso.vinci@polytechnique.edu>
  *
@@ -30,18 +30,10 @@
 #include "ui_nVISAR2.h"
 #include "ui_nVISAR3.h"
 
-#include <qwt_plot.h>
-#include <qwt_plot_curve.h>
-#include <qwt_plot_marker.h>
-#include <qwt_plot_picker.h>
-#include <qwt_picker_machine.h>
-
 #ifndef __nVisar
 #define __nVisar
 
 #include "nPhysWave.h"
-
-#include "nVisarZoomer.h"
 
 class neutrino;
 class nLine;
@@ -51,73 +43,76 @@ template<class T>
 inline T SIGN(T x) { return (x > 0) ? 1 : ((x < 0) ? -1 : 0); }
 
 class nVisar : public nGenericPan {
-	Q_OBJECT
-	
+    Q_OBJECT
+
     using nGenericPan::loadSettings;
 
 public:
-	
-	nVisar(neutrino *, QString);
-	Ui::nVisar my_w;
 
-	Ui::nVISAR2 visar[2];
-	Ui::nVISAR3 setvisar[2];
+    nVisar(neutrino *, QString);
+    Ui::nVisar my_w;
+
+    Ui::nVISAR2 visar[2];
+    Ui::nVISAR3 setvisar[2];
 
 public slots:
-	
-	void doWave();
-	void doWave(int);
 
-	void getCarrier();
-	void getCarrier(int);
-	
-	void getPhase();
-	void getPhase(int);
+    void doWave();
+    void doWave(int);
 
-	void updatePlot();
+    void getCarrier();
+    void getCarrier(int);
 
-	int direction(int);
+    void getPhase();
+    void getPhase(int);
 
-	void export_txt();
-	void export_txt_multiple();
-	
-	QString export_one(int);
-	QString export_sop();
-	QString export_plot(QwtPlot*);
-	void export_pdf();
-	void export_clipboard();
+    void updatePlot();
 
-	void connections();
-	void disconnections();
-	
-	void updatePlotSOP();
+    int direction(int);
 
-	void tabChanged(int=0);
-	
-	void mouseAtMatrix(QPointF);
+    void export_txt();
+    void export_txt_multiple();
 
-	void loadSettings(QString);
-	
+    QString export_one(int);
+    QString export_sop();
+//    QString export_plot(QwtPlot*);
+    QString export_plot();
+    void export_pdf();
+    void export_clipboard();
+
+    void connections();
+    void disconnections();
+
+    void updatePlotSOP();
+
+    void tabChanged(int=0);
+
+    void mouseAtMatrix(QPointF);
+
+    void loadSettings(QString);
+
     void bufferChanged(nPhysD*);
     
 private:
-	QwtPlotCurve cPhase[2][2],cIntensity[2][2],cContrast[2][2];
-    
-    QwtPlotCurve velocity[2],reflectivity[2], sopCurve[4];
-        
-    QList<QwtPlotMarker*> marker;
-	QList<QwtPlotCurve*> velJumps;	
-	
-	QwtPlotMarker mouseMarker[4];
-	QPointer<nVisarZoomer> zoomer[4];
-	
-	nPhysD phase[2][2];
-	nPhysD contrast[2][2];
-	nPhysD intensity[2][2];
-	
-	QPointer<nLine> fringeLine[2];
-	QPointer<nRect> fringeRect[2];
-	QPointer<nRect> sopRect;
+
+    QVector<double> cPhase[2][2], cIntensity[2][2], cContrast[2][2];
+    QVector<double> time_phase[2];
+
+    QVector<double> velocity[2], reflectivity[2];
+    QVector<double> time_vel[2];
+
+    QVector<double> sopCurve[4];
+    QVector<double> time_sop;
+
+    QPointer<QCPItemLine> mouseMarker[4];
+
+    nPhysD phase[2][2];
+    nPhysD contrast[2][2];
+    nPhysD intensity[2][2];
+
+    QPointer<nLine> fringeLine[2];
+    QPointer<nRect> fringeRect[2];
+    QPointer<nRect> sopRect;
 };
 
 #endif
